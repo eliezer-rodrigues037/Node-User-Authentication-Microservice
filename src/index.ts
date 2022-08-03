464c8c969b6700000f1684dcc73119c7bb7fbf47
eliezer-rodrigues037 <eliezer_gt.02@hotmail.com>
2022-08-03 13:11:21 -0300

    TS development flux.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Yoo!!");
+import  express,{Request, Response, NextFunction} from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/status", (req: Request,res: Response, next: NextFunction) => {
+    return res.status(200).send(({message: "Server is running"}));
+});
+
+app.listen(port, ()=> {
+    console.log(`Server running on port ${port}.`);
+})
